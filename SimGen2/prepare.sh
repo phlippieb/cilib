8ae752558c4d5bb6157e1d26020c425fc78f7fcc
Phlippie Bosman <phlippie.bosman@gmail.com>
2014-07-22 09:58:55 +0200

    added netbeans project files; modified config files for current work

--- SimGen2/prepare.sh
+++ SimGen2/prepare.sh
@@ -1,4 +1,3 @@
-clear
 ant jar -q
 if [ $? == 0 ]
 then 
@@ -6,6 +5,7 @@ then
 	java -jar dist/SimGen3.jar
 	if [ $? == 0 ]
 	then 
+		echo "Copying file to cilib directory"
 		cp sims.xml ../simulator/xml/
 	fi
 fi
