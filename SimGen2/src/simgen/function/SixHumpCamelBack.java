aafbea4b1fd493ceb93138f0c9f7fe7fed2f02a5
Phlippie Bosman <phlippie.bosman@gmail.com>
2014-10-08 23:33:45 +0200

    added fixed-dimensions functions Goldstein-Price and 6-hump to SimGen

--- SimGen2/src/simgen/function/SixHumpCamelBack.java
+++ SimGen2/src/simgen/function/SixHumpCamelBack.java
@@ -4,7 +4,7 @@ package simgen.function;
  * !! Note: restricted to 2 dimensions!
  */
 
-public class SixHumpCamelBack extends Function {
+public class SixHumpCamelBack extends FunctionWithFixedDimensions {
 	public void setName() {
 		this.name = "sixhump";
 	}
@@ -13,6 +13,13 @@ public class SixHumpCamelBack extends Function {
 		this.domainMin = -5;
 		this.domainMax =  5;
 	}
+
+    @Override
+    void setDimensions() {
+        this.dimensions = 2;
+    }
+        
+        
 
 	public void setDefinition() {
         this.definition = "<function class=\"functions.continuous.unconstrained.SixHumpCamelBack\"/>";
